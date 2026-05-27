44a60e22618763891d74558728716c6193062120
Omar Gomez Gomez <ogomez@kdu.cl>
2026-05-26 23:22:43 -0400

    feat(CreateService, TableSubscriptions): refactor components for improved readability and structure

--- src/components/TableSubscriptions.tsx
+++ src/components/TableSubscriptions.tsx
@@ -1,50 +1,51 @@
-import { Table, TableProps } from "antd";
-import { FC, useEffect, useState } from "react";
-import { CardStyle } from "./CardStyle";
-import { PageResponse } from "../types";
+import {Table, TableProps} from "antd";
+import {FC, useEffect, useState} from "react";
+import {CardStyle} from "./CardStyle";
+import {PageResponse} from "../types";
 import apiClient from "../services/apiClient";
 
-interface Subscriptions {}
+interface Subscriptions {
+}
 
 const columns: TableProps<Subscriptions>["columns"] = [
-  {
-    title: "N. Servicio",
-    dataIndex: "serviceNumber",
-    key: "serviceNumber",
-  },{
-    title: "Sector",
-    dataIndex: "sector.name",
-    key: "sector",
-  },
-  {
-    title: "Dirección",
-    dataIndex: "address",
-    key: "address",
-  },
-  {
-    title: "Titular",
-    dataIndex: "owner.name",
-    key: "owner.name",
-  },
+    {
+        title: "N. Servicio",
+        dataIndex: "serviceNumber",
+        key: "serviceNumber",
+    }, {
+        title: "Sector",
+        dataIndex: ["sector", "name"],
+        key: "sectorName",
+    },
+    {
+        title: "Dirección",
+        dataIndex: "address",
+        key: "address",
+    },
+    {
+        title: "Titular",
+        dataIndex: ["owner", "fullName"],
+        key: "ownerName",
+    },
 ];
 
 export const TableSubscriptions: FC = () => {
-    const [datasource, setdatasource] = useState<PageResponse<Subscriptions>>();
+    const [datasource, setDatasource] = useState<PageResponse<Subscriptions>>();
 
     useEffect(() => {
-    const fetch = async () => {
-        const { data} = await apiClient.get<PageResponse<Subscriptions>>("/api/subscriptions");
-        setdatasource( data);
-    };
-    fetch();
-  }, []);
-  return (
-    <CardStyle>
-      <Table<Subscriptions>
-        columns={columns}
-        rowKey="serviceNumber"
-        dataSource={datasource?.content}
-      />
-    </CardStyle>
-  );
+        const fetch = async () => {
+            const {data} = await apiClient.get<PageResponse<Subscriptions>>("/api/subscriptions");
+            setDatasource(data);
+        };
+        fetch();
+    }, []);
+    return (
+        <CardStyle>
+            <Table<Subscriptions>
+                columns={columns}
+                rowKey="serviceNumber"
+                dataSource={datasource?.content}
+            />
+        </CardStyle>
+    );
 };
